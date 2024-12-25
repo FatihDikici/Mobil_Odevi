b55ae67e56e5fd3d12a50ee509e2c68d1b30b964
FatihDikici <104261606+FatihDikici@users.noreply.github.com>
2024-12-25 20:25:12 +0300

    6.Commit
    store.js sayfası yapıldı

---
+++ store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from "./userSlice";
+
+
+
+import {thunk} from "redux-thunk";
+
+export const store = configureStore({
+    reducer: {
+        user: userReducer,
+      },
+
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
+  })
