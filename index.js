d975e7d8d6b02c178b46f2faf8990244f6502804
FatihDikici <104261606+FatihDikici@users.noreply.github.com>
2024-12-28 19:57:51 +0300

    9.Commit
    index sayfası güncellendi

---
+++ index.js
@@ -0,0 +1,20 @@
+import LoginPage from './LoginPage'
+import SignupPage from './SignupPage'
+import HomePage from './HomePage'
+import ProfilePage from './ProfilePage'
+import LabPage from './LabPage'
+import AdminPage from './AdminPage'
+import SearchPage from './SearchPage'
+import AnalysisPage from './AnalysisPage'
+import FastLabPage from './FastLabPage'
+export {
+    LoginPage, 
+    SignupPage, 
+    HomePage, 
+    ProfilePage,
+    LabPage,
+    AdminPage,
+    SearchPage,
+    AnalysisPage,
+    FastLabPage
+}
